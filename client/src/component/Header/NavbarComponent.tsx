8d1a9bd8ae96dc25093dae0e90ef252524edc786
aniket2727 <aniketkadam5950@gmail.com>
2024-10-25 16:22:16 +0530

    add code

--- client/src/component/Header/NavbarComponent.tsx
+++ client/src/component/Header/NavbarComponent.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './NavbarComponent.css'; // Import the normal CSS
+import './NavbarComponent.css'; 
 import Nabarbuttons from './Nabarbuttons';
 const NavbarComponent: React.FC = () => {
    
     return (
         <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center flex-wrap"> {/* Added flex-wrap for responsiveness */}
+            <div className="container mx-auto flex justify-between items-center flex-wrap"> 
                 <h1 className="text-white text-xl font-bold">Village Development Programme</h1>
                  <Nabarbuttons/>
             </div>
