756898b74b954e48a5f19d970c450deb5ac74530
aniket2727 <aniketkadam5950@gmail.com>
2024-11-07 15:42:33 +0530

    add code

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const Homepage: React.FC = () => {
+    const navigate=useNavigate();
     return (
         <div className="flex h-screen bg-gray-100">
             {/* Left-aligned column for buttons */}
             <div className="flex flex-col items-start ml-8 mt-4 space-y-4">
                 {/* Button styles */}
-                <button className="w-48 py-3 px-5 text-white bg-blue-500 hover:bg-blue-600 rounded-lg shadow-lg focus:outline-none">
+                <button  onClick={()=>navigate('/issue')} className="w-48 py-3 px-5 text-white bg-blue-500 hover:bg-blue-600 rounded-lg shadow-lg focus:outline-none">
                     Raise Issue
                 </button>
-                <button className="w-48 py-3 px-5 text-white bg-green-500 hover:bg-green-600 rounded-lg shadow-lg focus:outline-none">
+                <button  onClick={()=>navigate('/tractproject')} className="w-48 py-3 px-5 text-white bg-green-500 hover:bg-green-600 rounded-lg shadow-lg focus:outline-none">
                     Track Projects
                 </button>
             </div>
