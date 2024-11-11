cb5354a94567178ed263e198a6106f1f338c0fae
aniket2727 <aniketkadam5950@gmail.com>
2024-11-11 11:43:40 +0530

    add code

---
+++ server/controller/projectDetailsController.js
@@ -0,0 +1,29 @@
+import projectDetailsData from '../database/projectDetailsSchema';
+
+export const projectDetailsController = async (req, resp) => {
+    const { projectname, projectmanagername, areaname, projecttype, projectcost } = req.body;
+
+    try {
+        // Create a new instance of the project details model with data from the request body
+        const projectdetailsSave = new projectDetailsData({
+            projectname,
+            projectmanagername,
+            areaname,
+            projecttype,
+            projectcost
+        });
+
+        // Save the new project details to the database
+        const result = await projectdetailsSave.save();
+
+        if (!result) {
+            return resp.status(404).json({ message: "Error in saving data" });
+        }
+
+        resp.status(201).json({ message: "Successfully added" });
+
+    } catch (error) {
+        console.error("Error saving project details:", error); // Log the error for debugging
+        resp.status(500).json({ message: "Internal server error" });
+    }
+};
